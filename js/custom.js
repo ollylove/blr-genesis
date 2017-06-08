e5dc105f731f02d61f9ffc33924747ff93c43d94
olly <olly@ollys-MacBook-Pro.local>
2017-06-08 16:22:46 +0100

    news page styled

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,21 @@
 $(document).ready(function () {
     $(".menu ul").on("click", function () {
         $(this).toggleClass("active");
+    });
+
+    $(".news-filter button").on("click", function (e) {
+        e.preventDefault();
+        $(".news-filter button").removeClass("all-active active");
+        $(this).addClass("active");
+        var tag = $(this).attr("data-tag");
+        var newsItem = $(".news-container .news-article");
+        console.log(tag);
+        newsItem.removeClass("active");
+        newsItem.each(function () {
+            if ($(this).hasClass(tag)) {
+                $(this).addClass("active")
+            }
+        });
+        $(".news-container .col-3").addClass("full-width");
     })
 });
